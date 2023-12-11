197c4607338cb5909de2272b10441186fcb6a21c
jancossio <jancossio@gmail.com>
2023-12-11 01:28:00 +0100

    Entrega 40

---
+++ src/app/characters/characters.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ComunicateService } from '../comunicate.service'
+
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-characters',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, CommonModule, RouterOutlet],
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.css'
+})
+export class CharactersComponent {
+
+  characters: any=null;
+
+  constructor(private comuService:ComunicateService, private router:Router){
+    this.getCharacters();
+  }
+
+  getCharacters(){
+    this.comuService.getRandomCharacters(12)
+    .subscribe(result => {this.characters = result})
+    
+  }
+}
---
+++ src/app/comunicate.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InvokeFunctionExpr } from '@angular/compiler';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComunicateService {
+
+  private characters = 'https://rickandmortyapi.com/api/character/';
+  private maxCharacters = 827;  
+
+  constructor(private httpClient:HttpClient) { }
+
+  getCharacters(){
+    let id="1,2,3,4,5"
+    return this.httpClient.get(this.characters+id);   
+  }
+
+  getRandomCharacters(numChars:number){
+    let id='';
+    let tmp=0;
+    for(let i=0; i<numChars; i++){
+      tmp = Math.floor(Math.random()*(this.maxCharacters-1)+1);
+      id += (String(tmp)+',');
+    }
+    return this.httpClient.get(this.characters+id);
+  }
+}
